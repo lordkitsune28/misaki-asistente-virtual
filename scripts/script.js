2b62d2c8d028c4f7e68adb7e664a97779d196025
carlos medina <carlosassasin1505@gmail.com>
2023-02-09 14:45:27 -0500

    reloj codigo mejorado

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,46 @@
 let actHora = () => {
     let fecha = new Date(),
-        horas = fecha.getHours(),
-        ampm,
-        minutos = fecha.getMinutes(),
-        semana = fecha.getDay(),
-        dia = fecha.getDate(),
-        mes = fecha.getMonth(),
-        año = fecha.getFullYear();
+       ampm 
     // segundos = fecha.getSeconds();
+    let reloj = [{
+        horas: horas = fecha.getHours(),
+        minutos : fecha.getMinutes(),
+        semana : fecha.getDay(),
+        dia : fecha.getDate(),
+        mes : fecha.getMonth(),
+        año : fecha.getFullYear()
+    }
+    ]
+
+    let h = 0;
 
     let semD = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
     let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "septiembre", "Octubre", "Noviembre", "Diciembre"]
 
-    if (horas >= 12) {
-        horas = horas - 12;
-        ampm = "PM";
-    } else if (horas == 0) {
-        horas = 12;
-        ampm = "AM";
-    } else {
-        ampm = "AM";
-    }
+    $.each(reloj, function (index, value) { 
 
-    if (minutos < 10) { minutos = "0" + minutos }
+        if (value.horas >= 12) {
+            h = value.horas - 12;
+            ampm = "PM";
+        } else if (value.horas == 0) {
+            h = 12;
+            ampm = "AM";
+        } else {
+            ampm = "AM";
+        }
 
-    $("#semana").text(`${semD[semana]}`);
-    $("#dia").text(`${dia}`);
-    $("#mes").text(`${meses[mes]}`);
-    $("#año").text(`${año}`);
-    $("#hora").text(`${horas}`);
-    $("#min").text(`${minutos}`);
-    $("#ampm").text(`${ampm}`);
+        if (value.minutos < 10) { value.minutos = "0" + value.minutos };
+        if (h < 10) { h = "0" + h };
 
-
-
-
+        $("#semana").text(`${semD[value.semana]}`);
+        $("#dia").text(`${value.dia}`);
+        $("#mes").text(`${meses[value.mes]}`);
+        $("#año").text(`${value.año}`);
+        $("#hora").text(`${h}`);
+        $("#min").text(`${value.minutos}`);
+        $("#ampm").text(`${ampm}`);
+         
+    });
 }
 
 $(document).ready(function () {
